41bbd266c8fbed8b051b23d836453266d521dfcb
dewmal <dewmalnilanka@gmail.com>
2015-08-04 11:38:56 +0530

    Add Logo

--- server/src/main/java/org/egreen/plantation/Start.java
+++ server/src/main/java/org/egreen/plantation/Start.java
@@ -1,15 +1,20 @@
 package org.egreen.plantation;
 
+import com.sun.jersey.spi.container.servlet.ServletContainer;
 import com.sun.jersey.spi.spring.container.servlet.SpringServlet;
 import org.egreen.plantation.pushserver.DataEndpoint;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.NetworkListener;
+import org.glassfish.grizzly.servlet.DefaultServlet;
 import org.glassfish.grizzly.servlet.ServletRegistration;
 import org.glassfish.grizzly.servlet.WebappContext;
 import org.glassfish.tyrus.server.Server;
 
+import javax.servlet.ServletConfig;
 import javax.websocket.DeploymentException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * The sample demonstrates how to make Jersey-Spring
@@ -20,9 +25,18 @@ import java.io.IOException;
  */
 public class Start {
 
+    final static String YOUR_PACKAGE_NAME_GOES_HERE = "where.ever.your.resource.package.happens.to.be";
+
     public static void main(String[] args) throws IOException, DeploymentException {
         // Initialize Grizzly HttpServer
+
+
+
+        //SelectorThread threadSelector = GrizzlyWebContainerFactory.create(BASE_URI, initParams);
+
+
         HttpServer server = new HttpServer();
+      //  server.
         NetworkListener listener = new NetworkListener("grizzly2", "localhost", 8090);
         server.addListener(listener);
 
@@ -31,9 +45,11 @@ public class Start {
         final ServletRegistration reg = ctx.addServlet("spring", new SpringServlet());
         reg.addMapping("/*");
         ctx.addContextInitParameter("contextConfigLocation", "spring-context.xml");
+        ctx.addContextInitParameter("com.sun.jersey.config.property.packages","org.egreen.plantation.api");
         ctx.addListener("org.springframework.web.context.ContextLoaderListener");
         ctx.addListener("org.springframework.web.context.request.RequestContextListener");
 
+        
 
         Server wsServer = new Server("localhost", 8091, "/", null, DataEndpoint.class);
         wsServer.start();
